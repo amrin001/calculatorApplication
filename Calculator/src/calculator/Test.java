6105c6056c3b302545d26e0e0a6ecc189ec30ae9
amrin001 <97206963+amrin001@users.noreply.github.com>
2022-06-29 17:42:24 +0800

    included BODMAS calculator function

--- Calculator/src/calculator/Test.java
+++ Calculator/src/calculator/Test.java
@@ -1,10 +1,61 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import functions.CalculatorFunctions;
+import functions.CalculatorProgram;
+
 public class Test {
-	
+
 	public static void main(String[] args) {
-		double sum = 3 * 3;
-		System.out.println(sum);
+//		String bodmas = "6+7*2-8+6/2-1";
+		Scanner scanner = new Scanner(System.in);
+		String bodmas = scanner.nextLine();
+		String[] tempArr = bodmas.split("");
+		ArrayList<String> arr = new ArrayList<>();
+		for (int i = 0; i < tempArr.length; i++)
+			arr.add(tempArr[i]);
+
+		System.out.println(arr);
+
+		// to do up * and /
+		for (int i = 0; i < arr.size(); i++) {
+			double result = 0;
+			if (arr.get(i).contains("*")) {
+				result = new CalculatorFunctions().multiplyFunction(Double.parseDouble(arr.get(i - 1)),
+						(Double.parseDouble(arr.get(i + 1))));
+				arr.set(i, Double.toString(result));
+				arr.remove(i - 1);
+				arr.remove(i);
+			} else if (arr.get(i).contains("/")) {
+				result = new CalculatorFunctions().divideFunction(Double.parseDouble(arr.get(i - 1)),
+						(Double.parseDouble(arr.get(i + 1))));
+				arr.set(i, Double.toString(result));
+				arr.remove(i - 1);
+				arr.remove(i);
+			}
+		}
+
+		System.out.println(arr);
+
+		// to do up + and -
+		while (arr.size() > 1) {
+			for (int j = 1; j < arr.size();) {
+				double result = 0;
+				result = new CalculatorProgram().calcProg(Double.parseDouble(arr.get(j - 1)),
+						(Double.parseDouble(arr.get(j + 1))), arr.get(j));
+				System.out.println(arr.get(j - 1) + " " + arr.get(j) + " " + (arr.get(j + 1)) + " = " + " " + result);
+				arr.set(j, Double.toString(result));
+				arr.remove(j - 1);
+				arr.remove(j);
+//				System.out.println(arr);
+				break;
+
+			}
+		}
+
+		System.out.println(arr);
 	}
 
 }
